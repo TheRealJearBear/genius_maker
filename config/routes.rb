e5402b41502bc86e0841f3a9d236912295cd5770
Jared Blake <jaredblake@Jareds-MacBook-Air.local>
2018-05-15 11:53:47 -0400

    added static text translate en/es

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
-  root 'homes#index'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :videos
-  resources :nasas, only: [:index]
-  resources :pictures
-  resources :users
-  resources :topics
-  resources :meetings
-  resources :conversations do
-    resources :messages
-  end
-
-  namespace :api do
-    namespace :v1 do
-      resources :videos, only: [:index]
-      resources :nasas, only: [:index]
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    root 'homes#index'
+    devise_for :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :videos
+    resources :nasas, only: [:index]
+    resources :pictures
+    resources :users
+    resources :topics
+    resources :meetings
+    resources :conversations do
+      resources :messages
     end
-  end
 
+    namespace :api do
+      namespace :v1 do
+        resources :videos, only: [:index]
+        resources :nasas, only: [:index]
+      end
+    end
+
+  end
 end
